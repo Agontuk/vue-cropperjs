c20dd936934939b6d55d8d9abc9bb185a99d33b2
Gonzalo Guevara <gonzarock@yahoo.com>
2017-02-21 14:51:07 -0300

    Simple file component example (.vue)
    Add a single file component example.

---
+++ example/src/Cropper.vue
@@ -0,0 +1,75 @@
+<template>
+
+  <div id="app">
+    <h2 style="margin: 0;">Vue CropperJS</h2>
+    <hr/>
+    <input type="file" name="image" accept="image/*"
+           style="font-size: 1.2em; padding: 10px 0;"
+           @change="setImage" />
+    <br/>
+    <div style="max-width: 900px; display: inline-block;">
+      <vue-cropper ref='cropper' :src=imgSrc alt="Source Image" :cropmove="cropImage" ></vue-cropper>
+    </div>
+    <img :src="cropImg" style="width: 200px; height: 150px; border: 1px solid gray" alt="Cropped Image" />
+    <br/>
+
+    <button @click="rotate">Rotate</button>
+  </div>
+
+</template>
+
+<script>
+  import VueCropper from 'vue-cropperjs';
+
+
+  export default {
+    components: {
+      VueCropper,
+    },
+    data() {
+      return {
+        imgSrc: '',
+        cropImg: '',
+      };
+    },
+    methods: {
+      setImage(e) {
+        const file = e.target.files[0];
+
+        if (!file.type.includes('image/')) {
+          // alert('Please select an image file');
+          return;
+        }
+
+        if (typeof FileReader === 'function') {
+          const reader = new FileReader();
+
+          reader.onload = (event) => {
+            console.log(event, 'event');
+            this.imgSrc = event.target.result;
+            // rebuild cropperjs with the updated source
+            console.log(this.$refs.cropper);
+            this.$refs.cropper.replace(event.target.result);
+          };
+
+          reader.readAsDataURL(file);
+        } else {
+          alert('Sorry, FileReader API not supported');
+        }
+      },
+      cropImage() {
+        // get image data for post processing, e.g. upload or setting image src
+        this.cropImg = this.$refs.cropper.getCroppedCanvas().toDataURL();
+      },
+      rotate() {
+        // guess what this does :)
+        this.$refs.cropper.rotate(90);
+      },
+    },
+  };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
